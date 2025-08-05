26573c04a3ab76868ef6c0f227567b0dc9dde65d
상현 <91441630+csh6779@users.noreply.github.com>
2025-08-06 00:28:10 +0900

    예외처리 dto수정
    예외처리시 object로 하면 역직렬화시 타입 오류가 발생할 수 있어 제너릭으로 수정

--- Dtos/Login/ServiceResponseDto.cs
+++ Dtos/Login/ServiceResponseDto.cs
@@ -3,9 +3,9 @@ namespace JwtApi.Dtos;
 public class ServiceResponseDto
 {
     public int StatusCode { get; set; }
-    public object Body { get; set; }
+    public T Body { get; set; }
 
-    public ServiceResponseDto(int statuscode, object body)
+    public ServiceResponseDto(int statuscode, T body)
     {
         StatusCode = statuscode;
         Body = body;
